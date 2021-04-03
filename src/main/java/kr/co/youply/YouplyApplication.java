4c24c56c449b51385e9223b5e72f76edebcec0e8
WOOSERK <wooserknim@gmail.com>
2021-04-04 00:09:33 +0900

    백엔드 생성

---
+++ src/main/java/kr/co/youply/YouplyApplication.java
@@ -0,0 +1,15 @@
+package kr.co.youply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouplyApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(YouplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/youply/YouplyApplicationTests.java
@@ -0,0 +1,15 @@
+package kr.co.youply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouplyApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
